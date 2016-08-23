9a9a10423ae41b73cbfb6a952dff0947046a55e0
Marty Fitchen and Tony Fabery <tony@>
2016-08-23 11:41:22 -0700

    add working files to find legislative-specific information and committee-specific information from the API call with nav styling

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    var url = 'http://congress.api.sunlightfoundation.com/committees?apikey=f354f26de02441e2a82ebf8efd0d9230';
+    return Ember.$.getJSON(url).then(function(responseJSON) {
+      return responseJSON.results;
+    });
+  },
+  
   actions: {
     zipLookup(params) {
       this.transitionTo('results', params.zip);
